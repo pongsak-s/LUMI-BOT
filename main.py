657534f63bbc3f9a1bed8aa77b5aa4afe717b6ff
Pongsak S <pongsak.s@gmail.com>
2023-03-07 16:18:24 +0700

    rewrite it to using bot command

--- main.py
+++ main.py
@@ -1,42 +1,40 @@
 import discord
 import os
+from discord.ext import commands
 from lumi import get_lumi_price, get_lumi_price_usd, get_lumi_series
 from plot import plot
 import base64
 import io
 from exchange import get_usdthb
 
-
 intents = discord.Intents.default()
-client = discord.Client(intents=intents)
+intents.message_content = True
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0}'.format(bot.user))
 
+@bot.command()
+async def justprice(ctx):
+    await ctx.send(get_lumi_price())
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command()
+async def price(ctx):
+    lumi_series = get_lumi_series()
+    lumi_usd = get_lumi_price_usd(lumi_series)
+    await ctx.send(get_lumi_price(lumi_usd))
+    lumi_thb="{:.2f}".format(lumi_usd* get_usdthb())
+    await ctx.send("ราคาไทย: "+lumi_thb+" บาท")
+    await ctx.send("one sec. we are fetching the chart ...... (v2)")
+    png_base64 = base64.b64encode(plot(lumi_series)).decode('ascii')
+    file = discord.File(io.BytesIO(base64.b64decode(png_base64)), filename="image.png")
+    await ctx.send(file=file)
 
-    if message.content.startswith('$justprice'):
-        await message.channel.send(get_lumi_price())
+@bot.command()
+async def test(ctx):
+    print(read_file())
+    write_file(33.31)
+    await ctx.send(get_lumi_price())
 
-    if message.content.startswith('$price'):
-        lumi_series = get_lumi_series()
-        lumi_usd = get_lumi_price_usd(lumi_series)
-        await message.channel.send(get_lumi_price(lumi_usd))
-        lumi_thb="{:.2f}".format(lumi_usd* get_usdthb())
-        await message.channel.send("ราคาไทย: "+lumi_thb+" บาท")
-        await message.channel.send("one sec. we are fectching the chart ......")
-        png_base64 = base64.b64encode(plot(lumi_series)).decode('ascii')
-        file = discord.File(io.BytesIO(base64.b64decode(png_base64)), filename="image.png")
-        await message.channel.send(file=file)
-
-    if message.content.startswith('$test'):
-        print(read_file())
-        write_file(33.31)
-        await message.channel.send(get_lumi_price())
-
-client.run(os.getenv('TOKEN'))
+bot.run(os.getenv('TOKEN'))
